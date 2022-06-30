3544d7e2b105a2dae92c602393be135531ac77e6
ramila <ramilac2006@hotmail.com>
2022-06-30 15:56:24 -0300

    iniciando exercicio do dia 1 bloco 5

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,23 @@
+//Crie uma função que mude o texto na tag <p>-----</p> para uma descrição de como você se vê daqui a 2 anos. 
+//(Não gaste tempo pensando no texto e sim realizando o exercício)
+let text = document.getElementsByClassName('center-content');
+
+for (let i= 0; i< text.length; i+=1) {
+    text[2].innerText = 'Primeiro'; 
+}
+
+// Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
+document.getElementById('center-content').style.backgroundColor = 'rgb green'
+
+
+
+//  Crie uma função que mude a cor do quadrado vermelho para branco.
+//  Crie uma função que corrija o texto da tag <h1>.
+//  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+//  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+//         */
+
+
+
+
